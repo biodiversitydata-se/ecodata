1edff96003550e677f80c11e23f882773f9930e7
magdziarek <ale.magdziarek@gmail.com>
2020-08-04 08:03:57 +0000

    add methods for person

--- grails-app/services/au/org/ala/ecodata/PersonService.groovy
+++ grails-app/services/au/org/ala/ecodata/PersonService.groovy
@@ -1,11 +1,44 @@
 package au.org.ala.ecodata
 
-import grails.transaction.Transactional
+import com.mongodb.*
+import org.grails.datastore.mapping.mongo.MongoSession
+import groovy.json.JsonSlurper
 
-@Transactional
+
 class PersonService {
 
-    def serviceMethod() {
+    def create(Map props) {
+        log.debug props
 
+        def person = new Person(props)
+        try {
+            person.save(failOnError: true)
+            log.debug "person saved"
+            return [status:'ok']
+        } catch (Exception e) {
+            e.printStackTrace()
+            // clear session to avoid exception when GORM tries to autoflush the changes
+            Person.withSession { session -> session.clear() }
+            def error = "Error creating person ${props.firstName} ${props.lastName} - ${e.message}"
+            log.error(error, e)
+            return [status:'error',error:error]
+        }
+    }
+
+    def get(String projectId){
+        log.debug "inside get"
+        log.debug projectId
+    }
+
+    def list() {
+        log.debug "inside list"
+        def list = []
+        def persons = params.includeDeleted ? Person.list() :
+            Person.find()  // should be findByProject(params.project) or something
+        persons.each { person ->
+            list.data << personService.toMap(person)
+        }
+        list.sort {it.lastName}
+        list
     }
 }
