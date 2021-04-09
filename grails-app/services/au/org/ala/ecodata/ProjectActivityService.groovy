9ac33d169d3c488c8eb1e9315c8bfa262eca8cb2
magdziarek <ale.magdziarek@gmail.com>
2021-04-09 06:59:26 +0000

    get more info about pActivity at levelOfDetail brief

--- grails-app/services/au/org/ala/ecodata/ProjectActivityService.groovy
+++ grails-app/services/au/org/ala/ecodata/ProjectActivityService.groovy
@@ -275,8 +275,9 @@ class ProjectActivityService {
 
         // for systematic monitoring - only retains basic info needed for homepage
         if (levelOfDetail == BRIEF) {
-            mapOfProperties.keySet().retainAll(["name", "projectActivityId", "_id", "alert", 
-            "allowPolygons", "allowLine", "allowPoints", "surveySiteOption", "methodUrl", "projectId", "allowSegmentMetadata"])
+            mapOfProperties.remove("sites");
+            // mapOfProperties.keySet().retainAll(["name", "projectActivityId", "_id", "alert", 
+            // "allowPolygons", "allowLine", "allowPoints", "surveySiteOption", "methodUrl", "projectId", "allowSegmentMetadata"])
         }
 
         String id = mapOfProperties["_id"].toString()
